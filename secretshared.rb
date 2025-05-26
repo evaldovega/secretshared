e28eedf67b189aaf2442b4388cee3ff3878ed844
evaldo.vega <evaldo.vega@egmail.com>
2025-05-26 14:28:10 -0500

    Update homepage and URL for Secretshared; remove outdated release files

--- secretshared.rb
+++ secretshared.rb
@@ -1,7 +1,7 @@
 class Secretshared < Formula
     desc "Cli para compartir secretos con todo el equipo devs"
-    homepage "https://github.com/evaldo/secretshared"
-    url "https://github.com/evaldo/secretshared/releases/download/v0.1.0/secretshared-macos.tar.gz"
+    homepage "https://github.com/evaldovega/secretshared"
+    url "https://github.com/evaldovega/secretshared/releases/download/productive/secretshared"
     sha256 "528859dd974e6ff8b0b38a3d04bcc5c76d50dbdb831cc7ec28f580d5b952a3f6 "  # Usa `shasum -a 256 archivo.tar.gz` para obtenerlo
     version "0.1.0"
   
